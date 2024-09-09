359dac3edf6d74fda662b3fd5595491dc55e68eb
Faheem <faheem.din@codingcops.com>
2024-09-09 15:18:20 +0500

    integrated index action for portfolio items

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,30 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.times do |blog|
+Blog.create!(
+    
+    title:"My Blog Posts #{blog}",
+    body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+)
+end
+puts "10 blogs created"
+5.times do |skill|
+Skill.create!(
+    
+    title:"Rails #{skill}",
+    percent_utilized:15
+)
+end
+puts "4 skills created"
+9.times do |item|
+    Portfolio.create!(
+       title:"Portfolio title #{item}",
+       subtitle: "My greate Services",
+    body:"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat",
+    main_image:"https://place-hold.it/400x600",
+    thumbn_image:"https://place-hold.it/300x250"
+       
+    )
+    end
+puts "9 portfolio items created"
